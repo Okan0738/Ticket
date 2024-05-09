54082edcc24de0e2da13360fb68d8a517df69b7e
Okan <co.okancelik@gmail.com>
2024-05-09 23:33:58 +0200

    added user

--- src/main/java/com/okan/controller/UserController.java
+++ src/main/java/com/okan/controller/UserController.java
@@ -1,2 +1,18 @@
-package com.okan.controller;public class UserController {
+package com.okan.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+
+
+    @GetMapping("/create")
+    public String createUser(Model model) {
+
+        return "/user/create";
+    }
 }
